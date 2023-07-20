d98827cab8dbd1673845a0e95b98fecaa16cdb2d
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-07-20 12:01:51 +0530

    Implemented the Work Experience Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
+
 import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
 
